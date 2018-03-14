f7dcfaeaf458f92f2ca95c2f8e69bbd221f401c8
Michael Adams <mtadams007@gmail.com>
2018-03-14 19:47:30 -0400

    trees can be felled

--- main.js
+++ main.js
@@ -5,9 +5,37 @@ const moveRight = document.getElementById('buttonRight');
 
 const itemForSale = document.getElementById('switchProduct');
 
+const themButton = document.getElementById('them');
 
+const poorTrees = document.getElementsByClassName('treeKiller');
 
+const usButton = document.getElementById('us');
 
+themButton.addEventListener('click', function() {
+  // poorTrees[0].classList.remove('lorax1');
+  poorTrees[0].classList.add('onceler1');
+  // poorTrees[1].classList.remove('lorax2');
+  poorTrees[1].classList.add('onceler2');
+  // poorTrees[2].classList.remove('lorax3');
+  poorTrees[2].classList.add('onceler3');
+  // poorTrees[3].classList.remove('lorax4');
+  poorTrees[3].classList.add('onceler4');
+  // poorTrees[4].classList.remove('lorax5');
+  poorTrees[4].classList.add('onceler5');
+})
+
+usButton.addEventListener('click', function() {
+  poorTrees[0].classList.remove('onceler1');
+  // poorTrees[0].classList.add('lorax1');
+  poorTrees[1].classList.remove('onceler2');
+  // poorTrees[1].classList.add('lorax2');
+  poorTrees[2].classList.remove('onceler3');
+  // poorTrees[2].classList.add('lorax3');
+  poorTrees[3].classList.remove('onceler4');
+  // poorTrees[3].classList.add('lorax4');
+  poorTrees[4].classList.remove('onceler5');
+  // poorTrees[4].classList.add('lorax5');
+})
 //Moves items from right to left to view
 moveLeft.addEventListener('click', function(e) {
   // debugger;
